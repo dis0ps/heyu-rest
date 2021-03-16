bebb333480705fb49ead562fae8bddf4019d2a45
dis0ps <dis0ps.email@gmail.com>
2021-03-15 23:30:19 -0400

    adds CORS support

--- heyu_rest.py
+++ heyu_rest.py
@@ -1,18 +1,25 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
 from x10_controller import x10_controller
 from flask import Flask
 from flask import abort, request, jsonify, g
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+
 heyu_path = os.environ.get("HEYU_PATH")
 if heyu_path is None:
     print("Set environment variable HEYU_PATH")
     sys.exit(1)
 
-@app.route('/<string:house_code>/<string:device_number>/', methods=['DELETE', 'POST'])
+
+@app.route('/<string:house_code>/<string:device_number>/', methods=['DELETE', 'POST', 'OPTIONS'])
 def heyu_on_off(house_code, device_number):
+
+    results = ""
 
     x10 = x10_controller(heyu_path)
     if request.method == "POST":
@@ -27,8 +34,12 @@ def heyu_on_off(house_code, device_number):
         device_number=device_number,
         method=request.method)
 
-@app.route('/<string:house_code>', methods=['DELETE'])
+
+@app.route('/<string:house_code>', methods=['DELETE', 'OPTIONS'])
 def heyu_all_off(house_code):
+
+    results = ""
+
     x10 = x10_controller(heyu_path)
     if request.method == "DELETE":
         results = x10.all_off(house_code)
@@ -38,13 +49,16 @@ def heyu_all_off(house_code):
         house_code=house_code,
         method=request.method)
 
+
 @app.errorhandler(405)
 def method_not_allowed(e):
     return jsonify(error="Method not allowed"), 405
 
+
 @app.errorhandler(404)
 def resource_not_found(e):
     return jsonify(error="Resource not found"), 404
+
 
 if __name__ == '__main__':
     app.run()
